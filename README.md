a0f7338566a3af6a36029db424942ee3bc3490a5
ben-kirnich <benjamin@kirnich.org>
2018-06-11 09:09:59 +0200

    v.0.1
    init

--- README.md
+++ README.md
@@ -1,14 +1,5 @@
+Hybrid Devops Demo to showcase Consistent Hybrid Devops with Azure and Azure Stack
+This Demo is providing the code for a sample asp.net web appliation based on the contoso sports sample including powershell scripts and arm templates to setup a VSTS continous deployment pipline for a hybrid scenario with azure and azure stack.
+The Setup and Demo guide can be found in the docs folder.
 
-# Contributing
-
-This project welcomes contributions and suggestions.  Most contributions require you to agree to a
-Contributor License Agreement (CLA) declaring that you have the right to, and actually do, grant us
-the rights to use your contribution. For details, visit https://cla.microsoft.com.
-
-When you submit a pull request, a CLA-bot will automatically determine whether you need to provide
-a CLA and decorate the PR appropriately (e.g., label, comment). Simply follow the instructions
-provided by the bot. You will only need to do this once across all repos using our CLA.
-
-This project has adopted the [Microsoft Open Source Code of Conduct](https://opensource.microsoft.com/codeofconduct/).
-For more information see the [Code of Conduct FAQ](https://opensource.microsoft.com/codeofconduct/faq/) or
-contact [opencode@microsoft.com](mailto:opencode@microsoft.com) with any additional questions or comments.
+Farm%2Fdeploy.json)
---
+++ src/ContosoWeb/Deployment/deploy2.ps1
@@ -0,0 +1,76 @@
+Param(
+[string]$CloudName,
+[string]$ApplicationId,
+[string]$ApplicationKey,
+[string]$DirectoryTenant,
+[string]$ResourceGroupName,
+[string]$BuildFileLocation,
+[string]$AppServicePlanSkuName,
+[string]$AppServicePlanName,
+[string]$Region,
+[string]$Applicationname,
+[string]$DatabaseServerName,
+[string]$instrumentationkey)
+	
+	
+$ResourceManagerEndpoint = "https://management.local.azurestack.external" 
+    $stackdomain = "local.azurestack.external"
+    $name = "AzureStack-Tenant"         
+ 
+    Write-Verbose "Retrieving endpoints from the $ResourceManagerEndpoint..." -Verbose
+
+    $endpoints = Invoke-RestMethod -Method Get -Uri "$($ResourceManagerEndpoint.ToString().TrimEnd('/'))/metadata/endpoints?api-version=2015-01-01" -ErrorAction Stop
+    $AzureKeyVaultDnsSuffix="vault.$($stackdomain)".ToLowerInvariant()
+    $AzureKeyVaultServiceEndpointResourceId= $("https://vault.$stackdomain".ToLowerInvariant())
+    $StorageEndpointSuffix = ($stackdomain).ToLowerInvariant()
+    $aadAuthorityEndpoint = $endpoints.authentication.loginEndpoint
+    $azureEnvironmentParams = @{
+            Name                                     = $name
+            ActiveDirectoryEndpoint                  = $endpoints.authentication.loginEndpoint.TrimEnd('/') + "/"
+            ActiveDirectoryServiceEndpointResourceId = $endpoints.authentication.audiences[0]
+            ResourceManagerEndpoint                  = $ResourceManagerEndpoint
+            GalleryEndpoint                          = $endpoints.galleryEndpoint
+            GraphEndpoint                            = $endpoints.graphEndpoint
+            GraphAudience                            = $endpoints.graphEndpoint
+            StorageEndpointSuffix                    = $StorageEndpointSuffix
+            AzureKeyVaultDnsSuffix                   = $AzureKeyVaultDnsSuffix
+            AzureKeyVaultServiceEndpointResourceId   = $AzureKeyVaultServiceEndpointResourceId
+            EnableAdfsAuthentication                 = $aadAuthorityEndpoint.TrimEnd("/").EndsWith("/adfs", [System.StringComparison]::OrdinalIgnoreCase)
+        } 
+    Add-AzureRmEnvironment @azureEnvironmentParams
+
+#catch {}
+
+Write-Verbose "RG $ResourceGroupName" -Verbose
+Write-Verbose "Loc $Region" -Verbose
+
+
+
+
+$credentials = New-Object System.Management.Automation.PSCredential ($ApplicationId, $(ConvertTo-SecureString $ApplicationKey -AsPlainText -Force))
+
+Login-AzureRmAccount -EnvironmentName $CloudName -ServicePrincipal -Credential $credentials -TenantId $DirectoryTenant
+get-azurermsubscription
+get-azurermresourcegroup -ResourceGroupName $ResourceGroupName -ev notPresent -ea 0
+
+if ($notPresent)
+{
+    write-Verbose "Creating Resource Group $ResourceGroupName" -verbose
+    new-azurermresourcegroup -Name $ResourceGroupName -Location $Region
+
+}
+Else
+{
+write-Verbose "Resource Group $ResourceGroupName already exists" -Verbose
+}
+
+$TemplateParameterObject = @{
+  'AppServicePlanName' = $AppServicePlanName;
+  'AppServicePlanSkuName' = $AppServicePlanSkuName;
+  '_artifactslocation' = $BuildFileLocation;
+  'WebAppName' =$Applicationname;
+'databaseservername' =$databaseservername;
+'instrumentationkey' =$instrumentationkey
+};
+
+new-azurermresourcegroupdeployment -name $Applicationname -ResourceGroupName $ResourceGroupName -templatefile ".\HybridDevops_Appservices-ASP.NET (PREVIEW)-CI/drop/deploy2.json" -TemplateParameterObject $TemplateParameterObject
